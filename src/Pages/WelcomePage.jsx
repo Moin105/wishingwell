346c56ce649f46452734526a2ccc83ba6892f1ca
Tamoor Shah <tamoorshah462@gmail.com>
2024-03-05 23:57:13 +0500

    Add welcome page

--- src/Pages/WelcomePage.jsx
+++ src/Pages/WelcomePage.jsx
@@ -3,8 +3,24 @@ import '../Styles/WelcomePage.css'
 function WelcomePage() {
     return (
         <div className="container">
-            <div className="banner-image">image</div>
-            
+            <div className="banner-image"></div>
+            <div className='welcome-text'>
+                <span> Welcome to SendWish</span>
+            </div>
+            <div className='desc-text'>
+                <span>Gifting made easy.</span>
+                <span>Perfect for gifting for all occasions.</span>
+
+            </div>
+            <div className='form-wrapper'>
+            <button className='primary-button'>
+                   Next
+            </button>
+            <button className='secondary-button'>
+                   Skip
+            </button>
+            </div>
+
         </div>
     )
 }
